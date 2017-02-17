3c4fc2f830871edaa9ca4570dd298b02f6deeda7
WTFlay <schrie_f@etna-alternance.net>
2017-02-17 15:29:17 +0000

    Increase code.

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,48 @@
 use std::env;
 use std::fs;
-use std::os::freebsd::fs::MetadataExt;
 
-#[macro_use] extern crate fomat_macros;
-
-fn get_files(dir_name: &str) -> Vec<String> {
-
-    let mut file_names = Vec::new();
-
-    if let Ok(entries) = fs::read_dir(dir_name) {
-        for entry in entries {
-            if let Ok(file) = entry {
-                if let Some(name) = file.path().as_path().to_str() {
-                    file_names.push(name.to_string());
-                }
-            }
-        }
-    }
-    file_names
+struct Options {
+    list: bool,
 }
 
-fn show_files(list_name: Vec<String>) {
-    for file_name in &list_name {
-        match fs::metadata(file_name) {
-            Ok(file_metadata)    => {
-                if file_metadata.is_dir() {
-                    println!("{} d {}", file_metadata.st_ino(), file_name);
-                } else {
-                    println!("{} - {}", file_metadata.st_ino(), file_name);
+fn get_files_in_dir<'a>(file_name: &'a str) -> Result<Vec<String>, &'a str> {
+    match fs::metadata(file_name) {
+        Ok(file_metadata) => {
+            if file_metadata.is_dir() {
+                let mut file_names = Vec::new();
+                if let Ok(entries) = fs::read_dir(file_name) {
+                    for entry in entries {
+                        if let Ok(file) = entry {
+                            if let Some(name) = file.path().as_path().to_str() {
+                                file_names.push(name.to_string());
+                            }
+                        }
+                    }
                 }
-            },
-            Err(err)    => {
-                println!("myls: {}: {}", file_name, err);
-            },
+                Ok(file_names)
+            } else {
+                Err(file_name)
+            }
+        },
+        Err(err) => {
+            println!("myls: {}: {}", file_name, err);
+            Err(file_name)
+        },
+    }
+}
+
+fn show_files(name_files: Vec<String>, options: &Options) {
+    for (index, name) in name_files.iter().enumerate() {
+        if options.list {
+            println!("{}", name);
+        } else {
+            if index > 0 {
+                print!(" ");
+            }
+            print!("{}", name);
+            if index == (name_files.len() - 1) {
+                print!("\n");
+            }
         }
     }
 }
@@ -40,21 +50,26 @@ fn show_files(list_name: Vec<String>) {
 fn main() {
     let input_file_names: Vec<String> = match env::args().count() {
         1   => { vec![".".to_string()] },
-        _   => { env::args().skip(1).collect() },
+        _   => { env::args().skip(1).map(|x| x.to_string()).collect() },
     };
+    let options = Options { list: false };
 
-    for file_name in &input_file_names {
-        match fs::metadata(file_name) {
-            Ok(file_metadata)    => {
-                if file_metadata.is_dir() {
-                    show_files(get_files(file_name));
-                } else {
-                    println!("{}", file_name);
+    let mut simple_files: Vec<String> = Vec::new();
+    for (index, file_name) in input_file_names.iter().enumerate() {
+        match get_files_in_dir(file_name) {
+            Ok(files) => {
+                if input_file_names.len() > 1 {
+                    print!("{}:\n", file_name);
+                }
+                show_files(files, &options);
+                if index < (input_file_names.len() - 1) {
+                    print!("\n");
                 }
             },
-            Err(err)    => {
-                println!("myls: {}: {}", file_name, err);
+            Err(file_name) => {
+                simple_files.push(file_name.to_string());
             },
         }
     }
+    show_files(simple_files, &options);
 }
