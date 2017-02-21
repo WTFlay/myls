f190a8db3145acfb59eb84109b9dae84b3a008b7
WTFlay <schrie_f@etna-alternance.net>
2017-02-21 18:24:49 +0000

    Add print_error for print easly error. Add show_long_file for show long format of file. Add get_charactere_type for return the specific charactere type of given filename.

--- src/myls.rs
+++ src/myls.rs
@@ -1,8 +1,14 @@
 use std::fs;
+use std::io::Error;
+use std::os::unix::fs::FileTypeExt;
 
 pub struct Options {
     pub list: bool,
     pub all: bool,
+}
+
+fn print_error(file_name: &str, err: Error) {
+    println!("myls: {}: {}", file_name, err);
 }
 
 /// Return vector with all files name contain in dir_name
@@ -33,7 +39,7 @@ pub fn get_files_in_dir(dir_name: &str) -> Option<Vec<String>> {
             }
         },
         Err(err) => {
-            println!("myls: {}: {}", dir_name, err);
+            print_error(dir_name, err);
             None
         },
     }
@@ -47,7 +53,7 @@ pub fn show_files(name_files: Vec<&str>, options: &Options) -> bool {
         // If option all is false and the filename begin by '.', continue for.
 
         if options.list {
-            println!("{}", name);
+            show_long_file(name);
         } else {
             if index > 0 {
                 print!(" ");
@@ -59,4 +65,42 @@ pub fn show_files(name_files: Vec<&str>, options: &Options) -> bool {
         }
     }
     name_files.len() > 0
+}
+
+fn get_charactere_type(name_file: &str) -> char {
+    match fs::metadata(name_file) {
+        Ok(metadata) => {
+            let filetype = metadata.file_type();
+            if filetype.is_dir() {
+                'd'
+            } else if filetype.is_symlink() {
+                'l'
+            } else if filetype.is_block_device() {
+                'b'
+            } else if filetype.is_char_device() {
+                'c'
+            } else if filetype.is_fifo() {
+                'p'
+            } else if filetype.is_socket() {
+                's'
+            } else {
+                '-'
+            }
+        },
+        Err(err) => {
+            print_error(name_file, err);
+            '?'
+        },
+    }
+}
+
+fn show_long_file(name_file: &str) {
+    // TODO Determine the permision of file
+
+    // TODO number of link
+    
+    // TODO owner name and group name
+    
+    // TODO print name without begin
+    print!("{} {}\n", get_charactere_type(name_file), name_file);
 }
